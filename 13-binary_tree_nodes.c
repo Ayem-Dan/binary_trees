b43aba35de16582348ab708ee730cb626615be70
Str0-codes <danielMMomodu@gmail.com>
2022-08-29 00:06:52 +0100

    update

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,41 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
- *
- * Return: the balance factor
- *         0 if tree is NULL
+ * binary_tree_nodes - counts nodes with at least one child
+ *@tree: the pointer to the tree
+ * Return: the number of nodes
  */
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	int height_l, height_r;
+size_t counter = 0;
 
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
-	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
-
-	return (height_l - height_r);
-}
-
-/**
- * binary_tree_height - measures the height of a binary tree
- * @tree: tree to measure the height of
- *
- * Return: height of the tree
- *         0 if tree is NULL
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t height_l = 0;
-	size_t height_r = 0;
-
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+if (tree == NULL)
+return (0);
+if (tree->right != NULL || tree->left != NULL)
+counter += binary_tree_nodes(tree->left) + 1
++ binary_tree_nodes(tree->right);
+return (counter);
 }
